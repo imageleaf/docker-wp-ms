6e4a566f2498854c68cf9ea7039e28d1480eeabc
Yehuda Deutsch <yeh@uda.co.il>
2017-11-30 16:49:28 +0200

    Initial commit for wp-ms docker installation
    A good acomplishment for my 31st BDay while being home sick
    You too can do something in @18Minuten

---
+++ wp-ms-init.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+ENV_FILES=("mariadb.env" "wordpress.env" "wp-ms-init.env")
+
+ok=1
+for env_file in ${ENV_FILES[@]}; do
+    if [ ! -e "${env_file}" ]; then
+        echo "ENV file ${env_file} is missing"
+        ok=0
+    fi
+done
+
+if [ "x${ok}" = "x0" ]; then
+    exit
+fi
+
+DOCKER_ARGS="-f docker-compose-dev.yml"
+
+echo 'Bringing up the Database'
+docker-compose ${DOCKER_ARGS} up -d db
+
+sleep 2
+
+echo 'Bringing up the Wordpress instance'
+docker-compose ${DOCKER_ARGS} up -d wordpress
+
+sleep 1
+
+# wp core multisite-install
+#   [--url=<url>]
+#   [--base=<url-path>]
+#   [--subdomains]
+#   --title=<site-title>
+#   --admin_user=<username>
+#   [--admin_password=<password>]
+#   --admin_email=<email>
+#   [--skip-email]
+#   [--skip-config]
+
+source wp-ms-init.env
+
+echo 'Setting up multi-site'
+docker-compose ${DOCKER_ARGS} run --rm cli wp core multisite-install \
+    --subdomains \
+    --url="${URL}" \
+    --title="${TITLE}" \
+    --admin_user="${ADMIN_USER}" --admin_password="${ADMIN_PASSWORD}" --admin_email="${ADMIN_EMAIL}" \
+    --skip-email \
+    --quiet
+
+echo 'Bringing up the Nginx HTTP server'
+docker-compose ${DOCKER_ARGS} up -d web
