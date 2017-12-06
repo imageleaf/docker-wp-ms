619c0aa7ab41be6810e93ec03feaa184ad91730a
Yehuda Deutsch <yeh@uda.co.il>
2017-12-06 23:48:43 +0200

    Fix variant Dockerfile

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+VERSION_LIST=('5.6' '7.0' '7.1')
+
+# Alpine based
 for version in '5.6' '7.0' '7.1'; do
-    for variant in 'apache' 'cli' 'fpm-alpine' 'fpm'; do
+    dirname="docker/php${version}/fpm-alpine"
+    mkdir -p "${dirname}"
+    base="php${version}-fpm-alpine"
+    dockerfile="${dirname}/Dockerfile"
+    echo -e "FROM wordpress:${base}\n" > "${dockerfile}"
+    echo "$(cat Dockerfile-alpine)" >> "${dockerfile}"
+done
+
+# Debian based
+for version in '5.6' '7.0' '7.1'; do
+    for variant in 'apache' 'fpm'; do
         dirname="docker/php${version}/${variant}"
         mkdir -p "${dirname}"
         base="php${version}-${variant}"
         dockerfile="${dirname}/Dockerfile"
         echo -e "FROM wordpress:${base}\n" > "${dockerfile}"
-        echo "$(cat Dockerfile)" >> "${dockerfile}"
+        echo "$(cat Dockerfile-debian)" >> "${dockerfile}"
     done
 done
