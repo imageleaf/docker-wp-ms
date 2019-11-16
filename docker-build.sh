7c2308b90be557aec487f78e786719b116119c45
Yehuda Deutsch <yeh@uda.co.il>
2019-11-16 15:08:01 +0200

    Add date tag for wordpress and unify tag format

--- docker-build.sh
+++ docker-build.sh
@@ -9,6 +9,10 @@ TARGET_TAG_VARIANT_MAP=(
     ['fpm-alpine']='fpm-alpine'
 )
 
+BASE_REGISTRY_REPO='wordpress'
+TARGET_REGISTRY_REPO='yehuda/wordpress'
+DATE_TAG=`date +%Y%m%d`
+
 # Build wordpress images
 for version in "${VERSION_LIST[@]}"; do
     for target_variant in "${!TARGET_TAG_VARIANT_MAP[@]}"; do
@@ -17,22 +21,23 @@ for version in "${VERSION_LIST[@]}"; do
         target_tag="php${version}-${target_variant}"
         if [[ "${base_variant}" == "cli" ]]; then
             base_tag="${base_variant}-php${version}"
-            target_tag="${target_variant}-php${version}"
         fi
-        echo "Creating Dockerfile for ${base_tag}"
+        echo "Creating Dockerfile for ${target_tag}"
         base_dockerfile="Dockerfile-${target_variant}"
         dockerfile_content=$(sed -e "s/PHP_VERSION/${version}/" "${base_dockerfile}")
-        docker pull "wordpress:${base_tag}"
-        new_image_tag="yehuda/wordpress:${target_tag}"
-        echo -e "FROM wordpress:${base_tag}\n${dockerfile_content}" | docker build -t "${new_image_tag}" - 2>&1 > "docker_build_${base_tag}.log"
+        docker pull "${BASE_REGISTRY_REPO}:${base_tag}"
+        new_image_tag="${TARGET_REGISTRY_REPO}:${target_tag}"
+        echo -e "FROM ${BASE_REGISTRY_REPO}:${base_tag}\n${dockerfile_content}" | docker build -t "${new_image_tag}" -f - . 2>&1 > "docker_build_${base_tag}.log"
         result=$?
         if [[ "x${result}" != "x0" ]]; then
             echo 1>&2 "Failed building image for ${target_tag}"
             echo 1>&2 "Image kept on local disk for inspection"
         else
             echo "Building image for ${target_tag} succeeded"
+            docker tag "${new_image_tag}" "${new_image_tag}-${DATE_TAG}"
+            docker push "${new_image_tag}-${DATE_TAG}"
             docker push "${new_image_tag}"
-            docker image rm "${new_image_tag}"
+            docker image rm "${new_image_tag}" "${new_image_tag}-${DATE_TAG}"
         fi
     done
 done
@@ -44,23 +49,22 @@ for version in "${VERSION_LIST[@]}"; do
         if [[ "${base_variant}" == "cli" ]]; then
             base_tag="${base_variant}-php${version}"
         fi
-        docker image rm "wordpress:${base_tag}"
+        docker image rm "${BASE_REGISTRY_REPO}:${base_tag}"
     done
 done
 
 
 # Build nginx image
 echo "Creating Dockerfile for nginx:alpine"
-today=$(date +%Y%m%d)
-docker build -t "yehuda/wp-nginx:${today}" nginx 2>&1 > "docker_build_nginx.log"
+docker build -t "yehuda/wp-nginx:${DATE_TAG}" nginx 2>&1 > "docker_build_nginx.log"
 result=$?
 if [[ "x${result}" != "x0" ]]; then
     echo 1>&2 "Failed building image for nginx:alpine"
     echo 1>&2 "Image kept on local disk for inspection"
 else
     echo "Building image for nginx:alpine succeeded"
-    docker tag "yehuda/wp-nginx:${today}" 'yehuda/wp-nginx:latest'
-    docker push "yehuda/wp-nginx:${today}"
+    docker tag "yehuda/wp-nginx:${DATE_TAG}" 'yehuda/wp-nginx:latest'
+    docker push "yehuda/wp-nginx:${DATE_TAG}"
     docker push 'yehuda/wp-nginx:latest'
-    docker image rm 'nginx:alpine' "yehuda/wp-nginx:${today}" 'yehuda/wp-nginx:latest'
+    docker image rm 'nginx:alpine' "yehuda/wp-nginx:${DATE_TAG}" 'yehuda/wp-nginx:latest'
 fi
