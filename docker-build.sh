b96ea633456953598681687ce2e29f2225f89c02
Yehuda Deutsch <yeh@uda.co.il>
2020-02-01 22:19:34 +0200

    Add CI

--- docker-build.sh
+++ docker-build.sh
@@ -11,7 +11,9 @@ TARGET_TAG_VARIANT_MAP=(
 
 BASE_REGISTRY_REPO='wordpress'
 TARGET_REGISTRY_REPO='yehuda/wordpress'
-DATE_TAG=`date +%Y%m%d`
+if [ -z ${IMAGE_TAG} ]; then
+  IMAGE_TAG=`date +%Y%m%d`
+fi
 
 # Build wordpress images
 for version in "${VERSION_LIST[@]}"; do
@@ -34,37 +36,25 @@ for version in "${VERSION_LIST[@]}"; do
             echo 1>&2 "Image kept on local disk for inspection"
         else
             echo "Building image for ${target_tag} succeeded"
-            docker tag "${new_image_tag}" "${new_image_tag}-${DATE_TAG}"
-            docker push "${new_image_tag}-${DATE_TAG}"
+            docker tag "${new_image_tag}" "${new_image_tag}-${IMAGE_TAG}"
+            docker push "${new_image_tag}-${IMAGE_TAG}"
             docker push "${new_image_tag}"
-            docker image rm "${new_image_tag}" "${new_image_tag}-${DATE_TAG}"
         fi
     done
 done
-
-for version in "${VERSION_LIST[@]}"; do
-    for target_variant in "${!TARGET_TAG_VARIANT_MAP[@]}"; do
-        base_variant=${TARGET_TAG_VARIANT_MAP[$target_variant]}
-        base_tag="php${version}-${base_variant}"
-        if [[ "${base_variant}" == "cli" ]]; then
-            base_tag="${base_variant}-php${version}"
-        fi
-        docker image rm "${BASE_REGISTRY_REPO}:${base_tag}"
-    done
-done
-
 
 # Build nginx image
 echo "Creating Dockerfile for nginx:alpine"
-docker build -t "yehuda/wp-nginx:${DATE_TAG}" nginx 2>&1 > "docker_build_nginx.log"
+docker build \
+  -t "yehuda/wp-nginx:${IMAGE_TAG}" \
+  -t "yehuda/wp-nginx:latest" \
+  nginx 2>&1 > "docker_build_nginx.log"
 result=$?
 if [[ "x${result}" != "x0" ]]; then
     echo 1>&2 "Failed building image for nginx:alpine"
     echo 1>&2 "Image kept on local disk for inspection"
 else
     echo "Building image for nginx:alpine succeeded"
-    docker tag "yehuda/wp-nginx:${DATE_TAG}" 'yehuda/wp-nginx:latest'
-    docker push "yehuda/wp-nginx:${DATE_TAG}"
+    docker push "yehuda/wp-nginx:${IMAGE_TAG}"
     docker push 'yehuda/wp-nginx:latest'
-    docker image rm 'nginx:alpine' "yehuda/wp-nginx:${DATE_TAG}" 'yehuda/wp-nginx:latest'
 fi
