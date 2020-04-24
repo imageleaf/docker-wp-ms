4f3a0ef67d985d47e75084788593c6fbfd580f83
Yehuda Deutsch <yeh@uda.co.il>
2020-04-25 01:08:05 +0200

    Separate version builds

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-VERSION_LIST=('7.3' '7.4')
+if [ -z ${BASE_VERSION} ]; then
+  echo >&2 Base version not specified
+  exit 1
+fi      
+
 declare -A TARGET_TAG_VARIANT_MAP
 TARGET_TAG_VARIANT_MAP=(
     ['cron']='cli'
@@ -11,37 +15,42 @@ TARGET_TAG_VARIANT_MAP=(
 
 BASE_REGISTRY_REPO='wordpress'
 TARGET_REGISTRY_REPO='yehuda/wordpress'
+BUILD_NGINX=yes
 if [ -z ${IMAGE_TAG} ]; then
   IMAGE_TAG=`date +%Y%m%d`
+  BUILD_NGINX=no
 fi
 
 # Build wordpress images
-for version in "${VERSION_LIST[@]}"; do
-    for target_variant in "${!TARGET_TAG_VARIANT_MAP[@]}"; do
-        base_variant=${TARGET_TAG_VARIANT_MAP[$target_variant]}
-        base_tag="php${version}-${base_variant}"
-        target_tag="php${version}-${target_variant}"
-        if [[ "${base_variant}" == "cli" ]]; then
-            base_tag="${base_variant}-php${version}"
-        fi
-        echo "Creating Dockerfile for ${target_tag}"
-        base_dockerfile="Dockerfile-${target_variant}"
-        dockerfile_content=$(sed -e "s/PHP_VERSION/${version}/" "${base_dockerfile}")
-        docker pull "${BASE_REGISTRY_REPO}:${base_tag}"
-        new_image_tag="${TARGET_REGISTRY_REPO}:${target_tag}"
-        echo -e "FROM ${BASE_REGISTRY_REPO}:${base_tag}\n${dockerfile_content}" | docker build -t "${new_image_tag}" -f - . 2>&1 > "docker_build_${base_tag}.log"
-        result=$?
-        if [[ "x${result}" != "x0" ]]; then
-            echo 1>&2 "Failed building image for ${target_tag}"
-            echo 1>&2 "Image kept on local disk for inspection"
-        else
-            echo "Building image for ${target_tag} succeeded"
-            docker tag "${new_image_tag}" "${new_image_tag}-${IMAGE_TAG}"
-            docker push "${new_image_tag}-${IMAGE_TAG}"
-            docker push "${new_image_tag}"
-        fi
-    done
+version=${BASE_VERSION}
+for target_variant in "${!TARGET_TAG_VARIANT_MAP[@]}"; do
+  base_variant=${TARGET_TAG_VARIANT_MAP[$target_variant]}
+  base_tag="php${version}-${base_variant}"
+  target_tag="php${version}-${target_variant}"
+  if [[ "${base_variant}" == "cli" ]]; then
+    base_tag="${base_variant}-php${version}"
+  fi
+  echo "Creating Dockerfile for ${target_tag}"
+  base_dockerfile="Dockerfile-${target_variant}"
+  dockerfile_content=$(sed -e "s/PHP_VERSION/${version}/" "${base_dockerfile}")
+  docker pull "${BASE_REGISTRY_REPO}:${base_tag}"
+  new_image_tag="${TARGET_REGISTRY_REPO}:${target_tag}"
+  echo -e "FROM ${BASE_REGISTRY_REPO}:${base_tag}\n${dockerfile_content}" | docker build -t "${new_image_tag}" -f - . 2>&1 > "docker_build_${base_tag}.log"
+  result=$?
+  if [[ "x${result}" != "x0" ]]; then
+    echo 1>&2 "Failed building image for ${target_tag}"
+    echo 1>&2 "Image kept on local disk for inspection"
+  else
+    echo "Building image for ${target_tag} succeeded"
+    docker tag "${new_image_tag}" "${new_image_tag}-${IMAGE_TAG}"
+    docker push "${new_image_tag}-${IMAGE_TAG}"
+    docker push "${new_image_tag}"
+  fi
 done
+
+if [ "${BUILD_NGINX}" == "no" ]; then
+  exit
+fi
 
 # Build nginx image
 echo "Creating Dockerfile for nginx:alpine"
